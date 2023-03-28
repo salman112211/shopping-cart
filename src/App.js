827a3d015b75c44a1f891516f58d848afdd423d3
salman112211 <ksalmantaj@gmail.com>
2023-03-28 15:04:49 +0500

    car app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,112 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import './index.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronRight, faChevronLeft, faCircle, faCheckCircle, faPlus } from '@fortawesome/free-solid-svg-icons';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+	// HINT: each "item" in our list names a name, a boolean to tell if its been completed, and a quantity
+	const [items, setItems] = useState([
+   {itemName: "Item 1" , quntity: 1, isSelected:false},
+   {itemName: "Item 2" , quntity: 3, isSelected:true},
+   {itemName: "Item 3" , quntity: 2, isSelected:false},
+   
+  ]);
+
+const [inputValue, setInputValue] =useState("");
+const [totalItemCount, setTotalItemCount] =useState(0);
+
+const handleAddButtonClick =()=>{
+  const newItem={
+    itemName: inputValue,
+    quntity:false,
+    isSelected:false,
+  }
+
+  const newItems= [...items, newItem];
+
+  setItems(newItems);
+  setInputValue("");
 }
+
+
+const handleIncrese =(index)=>{
+  const newItems=[...items]
+  newItems[index].quntity ++;
+    setItems(newItems);
+    calculateTotal();
+}
+
+
+const handleDecrese=(index)=>{
+  const newItems=[...items]
+  newItems[index].quntity--;
+
+   setItems(newItems);
+  calculateTotal();
+}
+
+
+const togleComplete =(index)=>{
+  const newItems=[...items]
+  newItems[index].isSelected=!newItems[index].isSelected
+
+  setItems(newItems);
+}
+
+
+const calculateTotal = ()=>{
+
+  const totalItemCount= items.reduce((total, item)=>{
+    return total+item.quntity;
+  },0);
+
+  setTotalItemCount(totalItemCount);
+}
+
+	return (
+		<div className='app-background'>
+			<div className='main-container'>
+				<div className='add-item-box'>
+					<input value={inputValue}  onChange={(event)=>setInputValue(event.target.value)}   className='add-item-input' placeholder='Add an item...' />
+					<FontAwesomeIcon icon={faPlus} onClick={()=>handleAddButtonClick()}/>
+				</div>
+				<div className='item-list'>
+          {items.map((item, index)=>
+          <div className='item-container'>
+						<div className='item-name' onClick={()=>togleComplete(index)}>
+							{/* HINT: replace false with a boolean indicating the item has been completed or not */}
+							{item.isSelected ? (
+								<>
+									<FontAwesomeIcon icon={faCheckCircle} />
+									<span className='completed'>{item.itemName}</span>
+								</>
+							) : (
+								<>
+									<FontAwesomeIcon icon={faCircle} />
+									<span>{item.itemName}</span>
+								</>
+							)}
+						</div>
+						<div className='quantity'>
+							<button>
+								<FontAwesomeIcon icon={faChevronLeft} onClick={()=>handleDecrese(index)} />
+							</button>
+							<span> {item.quntity} </span>
+							<button>
+								<FontAwesomeIcon icon={faChevronRight} onClick={()=>handleIncrese(index)} />
+							</button>
+						</div>
+					</div>
+          
+          
+          
+          )}
+					
+				</div>
+				<div className='total'>Total: {totalItemCount}</div>
+			</div>
+		</div>
+	);
+};
 
 export default App;
